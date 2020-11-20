ef7d2505ce1b99f343d01a286f8e25ec1e817656
Александра Молчанова <annakarmolina@icloud.com>
2020-11-20 11:25:36 +0300

    Добавьте файлы проекта.

---
+++ B18/B18.cpp
@@ -0,0 +1,107 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<Windows.h>
+#include<windowsx.h>
+#include<tchar.h>
+#define MI_BLUE 100
+#define MI_GREEN 101
+#define MI_EXIT 102
+
+HBRUSH hYellowBrush, hBrownBrush;
+int sw;
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	HWND hMainWnd;
+	TCHAR szClassName[] = _T("menudemo");
+	MSG msg;
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(wc));
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)CreateSolidBrush(RGB(245, 242, 122));
+	wc.lpszMenuName = _T("Menu");
+	wc.lpszClassName = szClassName;
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	if (!RegisterClassExW(&wc)) {
+		MessageBoxW(NULL, _T("Cannot register class"), _T("Error"), MB_OK);
+		return 0;
+	}
+	hMainWnd = CreateWindow(szClassName, _T("menudemo"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL,
+		hInstance, NULL);
+
+	if (!hMainWnd) {
+		MessageBox(NULL, _T("Cannot create main window"), _T("Error"), MB_OK);
+		return 0;
+	}
+	ShowWindow(hMainWnd, nCmdShow);
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessageW(&msg);
+	}
+	return 0;
+}
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	HDC hdc;
+	PAINTSTRUCT ps;
+	HMENU hMenu, hPopupMenu;
+	TCHAR str0[] = _T("Булочки");
+	TCHAR str1[] = _T("Пышка");
+	TCHAR str2[] = _T("Бублик");
+	switch (uMsg) {
+	case WM_COMMAND:
+		hMenu = GetMenu(hWnd);
+		switch (LOWORD(wParam))
+		{
+		case MI_BLUE:
+			sw = 1;
+			InvalidateRect(hWnd, NULL, TRUE);
+			break;
+		case MI_GREEN:
+			sw = 2;
+			InvalidateRect(hWnd, NULL, TRUE);
+			break;
+		case MI_EXIT:
+			DestroyWindow(hWnd);
+		}
+		break;
+	case WM_CREATE:
+		hYellowBrush = CreateSolidBrush(RGB(245, 242, 122));
+		hBrownBrush = CreateSolidBrush(RGB(131, 85, 29));
+		hMenu = CreateMenu();
+		hPopupMenu = CreatePopupMenu();
+		AppendMenu(hPopupMenu, MF_STRING, MI_BLUE, str1);
+		AppendMenu(hPopupMenu, MF_STRING, MI_GREEN, str2);
+		AppendMenu(hMenu, MF_POPUP, (UINT)hPopupMenu, str0);
+		SetMenu(hWnd, hMenu);
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		if (sw == 1)
+		{
+			SelectBrush(hdc, hBrownBrush);
+			Ellipse(hdc,300, 50, 600, 350 );
+
+		}
+		if (sw == 2)
+		{
+			SelectBrush(hdc, hBrownBrush);
+			Ellipse(hdc, 300, 50, 600, 350);
+			SelectBrush(hdc, hYellowBrush);
+			Ellipse(hdc, 400, 150, 500, 250);
+		}
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
+	return 0;
+
+}
